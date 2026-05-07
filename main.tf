13142acde8327349082792544065810e94268b73
abhirajyadav786 <abhirajyadav786@gmail.com>
2026-05-08 01:24:56 +0530

    test2

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "resource_group" {
-  source   = "./terraformModules/modules/resource_group"
+  source   = "./terraformModules/modules/RG"
   name     = var.resource_group_name
   location = var.location
   tags     = var.tags
