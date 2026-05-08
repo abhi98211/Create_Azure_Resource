6751f801a8bfc97ff46f3b937576764a27721c2a
abhirajyadav786 <abhirajyadav786@gmail.com>
2026-05-08 09:26:04 +0530

    ukgjv

--- main.tf
+++ main.tf
@@ -9,11 +9,17 @@ module "virtual_network" {
   source              = "./terraformModules/modules/Vnet"
   vnet_name           = "example-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = module.resource_group.location
-  resource_group_name = module.resource_group.name
-  subnets = {
-        name           = "example-subnet"
-        address_prefix = "10.0.1.0/24"
-    }
+  location            = module.resource_group.resource_group_location
+  resource_group_name = module.resource_group.resource_group_name
+  subnets = [{
+        
+        name  = "example-subnet"
+        address_prefix = ["10.0.1.0/24"]
+        },
+        {
+        name   = "example-subnet2"
+        address_prefix = ["10.0.2.0/24"]
+        }
+  ]
 }
 
