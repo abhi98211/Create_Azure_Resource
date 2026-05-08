4cbf96dd6b1eaa2e794542f48948dafb30dd57bd
abhirajyadav786 <abhirajyadav786@gmail.com>
2026-05-08 10:15:06 +0530

    jkvbhvj

--- variables.tf
+++ variables.tf
@@ -24,4 +24,14 @@ variable "subnets" {
     name = string
     address_prefixes = list(string)
   }))
+  default = {
+    example-subnet1 = {
+      name             = "example-subnet1"
+      address_prefixes = ["10.0.1.0/24"]
+    }
+    example-subnet2 = {
+      name             = "example-subnet2"
+      address_prefixes = ["10.0.2.0/24"]
+    }
+  }
 }
