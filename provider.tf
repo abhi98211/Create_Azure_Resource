f8c579d0319fe98d9b82fb17179c315916ca0065
abhirajyadav786 <abhirajyadav786@gmail.com>
2026-05-08 12:56:17 +0530

    fdfdf

--- provider.tf
+++ provider.tf
@@ -11,7 +11,7 @@ terraform {
 provider "azurerm" {
     subscription_id = "10d5d510-e3f2-4723-a675-7e2c9ff55b8d"
     tenant_id = "d7b0e3f5-2f4e-481a-a5f6-66bfb6a98055"
-    client_id = "bb5c1baa-b47b-4275-b9d5-e35b310f7ccd"
+    #client_id = "bb5c1baa-b47b-4275-b9d5-e35b310f7ccd"
     use_oidc = true
   features {}
 }
